f52cda481eb0df1842b3d067740c10a3a4ed1bd7
Deimon Willers <to.dwillers@gmail.com>
2018-12-26 16:52:19 -0200

    Added another value on values controller

--- service1/Controllers/ValuesController.cs
+++ service1/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace service1.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "Value 3" };
         }
 
         // GET api/values/5
